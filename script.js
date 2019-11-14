05d0144849fa9d0b06ce5d2258843763c7ede7ec
Nelly Sugu <jonly03@gmail.com>
2019-11-14 01:08:02 -0400

    Calculate vh with window height

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ $(document).ready(() => {
 
     $("#opening").addClass('animate');
 
+    let vh = $(window).height();
+    let bodyH = 96 * vh / 100
+    $("body").css("height", `${bodyH}px`)
+
 })
