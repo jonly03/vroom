9ec4870c29a9702cc0513c8fe7c534ee77a7e8e9
Nelly Sugu <jonly03@gmail.com>
2020-02-06 12:04:20 +0200

    Now open

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 $(document).ready(() => {
+  $("#opening").addClass("animate");
 
-    $("#opening").addClass('animate');
-
-    let vh = $(window).height();
-    let bodyH = 96 * vh / 100
-    $("body").css("height", `${bodyH}px`)
-
-})
+  let vh = $(window).height();
+  let bodyH = (96 * vh) / 100;
+  $("body").css("height", `${bodyH}px`);
+});
